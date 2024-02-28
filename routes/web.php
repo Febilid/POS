8782d5a6555126cde6f78b3fe6eff248f7529425
Febilid <febiolalidyasianturi@gmail.com>
2024-02-28 21:11:41 +0700

    Perubahan 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\BabyKidController;
+use App\Http\Controllers\BeautyHealthController;
+use App\Http\Controllers\BuyerUserController;
+use App\Http\Controllers\FoodBevarageController;
+use App\Http\Controllers\HomeCareController;
+use App\Http\Controllers\SaleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +20,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::get('/home', [HomeController::class, 'index']);
+
+Route::prefix('category')->group(function () {
+    Route::get('/foodbeverage', [FoodBevarageController::class, 'index']);
+    Route::get('/homecare', [HomeCareController::class, 'index']);
+    Route::get('/babykid', [BabyKidController::class, 'index']);
+    Route::get('/beautyhealth', [BeautyHealthController::class, 'index']);
 });
+
+
+Route::get('/id/{id}/name/{name}', [BuyerUserController::class, 'index']);
+
+Route::get('/Sale', [SaleController::class, 'index']);
